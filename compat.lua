fbafa3b7de2b9aaa54375594a08abfedbc9b7a23
TheRealNova422-real <polaris422@outlook.com>
2025-04-21 15:20:02 +1000

    everything else

--- compat.lua
+++ compat.lua
@@ -2,10 +2,8 @@
 
 -- Things that are not yet included:
 -- Banned cards in challenges (I think these need a full update anyway)
--- POINTER changes (I want to make an easier way for mods to update these)
--- Gameset patches
 
--- Take ownership calls
+-- encoded handling
 SMODS.Back:take_ownership("b_cry_encoded", {
 	apply = function(self)
 		G.GAME.joker_rate = 1
@@ -31,84 +29,8 @@ SMODS.Back:take_ownership("b_cry_encoded", {
 	end,
 })
 
-Cryptid.pointerblistify("j_jen_kosmos") -- here as an example
-Cryptid.pointerblistifytype("rarity", "jen_wondrous")
-Cryptid.pointerblistifytype("rarity", "jen_extraordinary")
-Cryptid.pointerblistifytype("rarity", "jen_ritualistic")
-Cryptid.pointerblistifytype("rarity", "jen_transcendent")
-Cryptid.pointerblistifytype("rarity", "jen_omegatranscendent")
-Cryptid.pointerblistifytype("rarity", "jen_omnipotent")
-Cryptid.pointerblistifytype("rarity", "jen_miscellaneous")
-Cryptid.pointerblistifytype("rarity", "jen_junk")
-Cryptid.pointerblistifytype("set", "jen_omegaconsumable")
 
-Cryptid.pointeraliasify("j_jen_kosmos", "Kosmos") -- also here as an example, this will have to be done for every alias (won't let you create kosmos because of blacklist)
-
-SMODS.Consumable:take_ownership("c_cry_gateway", {
-    can_use = function(self, card)
-		if (#SMODS.find_card("j_jen_saint") + #SMODS.find_card("j_jen_saint_attuned")) > 0 then
-			return #G.jokers.cards < G.jokers.config.card_limit
-		else
-			--Don't allow use if everything is eternal and there is no room
-			return #Cryptid.advanced_find_joker(nil, nil, nil, { "eternal" }, true, "j") < G.jokers.config.card_limit
-		end
-	end,
-	use = function(self, card, area, copier)
-		if (#SMODS.find_card("j_jen_saint") + #SMODS.find_card("j_jen_saint_attuned")) <= 0 then
-			local deletable_jokers = {}
-			for k, v in pairs(G.jokers.cards) do
-				if not v.ability.eternal then
-					deletable_jokers[#deletable_jokers + 1] = v
-				end
-			end
-			local _first_dissolve = nil
-			G.E_MANAGER:add_event(Event({
-				trigger = "before",
-				delay = 0.75,
-				func = function()
-					for k, v in pairs(deletable_jokers) do
-						if v.config.center.rarity == "cry_exotic" then
-							check_for_unlock({ type = "what_have_you_done" })
-						end
-						v:start_dissolve(nil, _first_dissolve)
-						_first_dissolve = true
-					end
-					return true
-				end,
-			}))
-		end
-		G.E_MANAGER:add_event(Event({
-			trigger = "after",
-			delay = 0.4,
-			func = function()
-				play_sound("timpani")
-				local card = create_card("Joker", G.jokers, nil, "cry_exotic", nil, nil, nil, "cry_gateway")
-				card:add_to_deck()
-				G.jokers:emplace(card)
-				card:juice_up(0.3, 0.5)
-				return true
-			end,
-		}))
-		delay(0.6)
-	end,
-})
-SMODS.Event:take_ownership("ev_cry_choco1", {
-    start = function(self)
-		G.GAME.events[self.key] = true
-		local areas = { "jokers", "deck", "hand", "play", "discard" }
-		for k, v in pairs(areas) do
-			for i = 1, #G[v].cards do
-				if pseudorandom(pseudoseed("cry_choco_possession")) < G.GAME.probabilities.normal / 3 then
-					SMODS.Stickers.cry_flickering:apply(G[v].cards[i], true)
-				end
-			end
-		end
-		--DON'T create a ghost
-		if G.GAME.dollars ~= 0 then
-            ease_dollars((-G.GAME.dollars - 1e6), true)
-        end
-	end,
-})
+Cryptid_config.gameset_toggle = false -- prevents changing gamesets. Easy.
 
 function Cryptid.gameset(card, center)
     return "madness"
@@ -125,8 +47,25 @@ function SMODS.current_mod.process_loc_text()
         },
     }
 end
+
+
+
+-- a guide on what these new functions do is in Cryptid/items/pointer.lua
+Cryptid.pointerblistify("j_jen_kosmos") -- here as an example
+Cryptid.pointerblistifytype("rarity", "jen_wondrous")
+Cryptid.pointerblistifytype("rarity", "jen_extraordinary")
+Cryptid.pointerblistifytype("rarity", "jen_ritualistic")
+Cryptid.pointerblistifytype("rarity", "jen_transcendent")
+Cryptid.pointerblistifytype("rarity", "jen_omegatranscendent")
+Cryptid.pointerblistifytype("rarity", "jen_omnipotent")
+Cryptid.pointerblistifytype("rarity", "jen_miscellaneous")
+Cryptid.pointerblistifytype("rarity", "jen_junk")
+Cryptid.pointerblistifytype("set", "jen_omegaconsumable")
+
+Cryptid.pointeraliasify("j_jen_kosmos", "Kosmos") -- also here as an example, this will have to be done for every alias (won't let you create kosmos because of blacklist)
+
+-- the old pointer thing, it's still case insensitive though (unless it's 1 letter long)
 local aliases = {
-	kosmos = "",
 	freddy = "freddy snowshoe",
 	paupovlin = "paupovlin revere",
 	poppin = "paupovlin revere",
@@ -192,6 +131,3 @@ local aliases = {
 	["sagitarius a*"] = "sagittarius a*", --minor spelling mistakes are forgiven
 	sagitarius = "sagittarius a*", --minor spelling mistakes are forgiven
 }
-for k, v in pairs(aliases) do
-	Cryptid.aliases[k] = v
-end
